8e2b3b86673bd565de679bd5ffcb2cfbd25ff8d3
fengxiaomao <fxm@autoforce.net>
2018-07-30 13:33:14 +0800

    add priority-queue test

--- src/data-structures/priority-queue/PriorityQueue.js
+++ src/data-structures/priority-queue/PriorityQueue.js
@@ -21,4 +21,69 @@ export default class PriorityQueue extends MinHeap {
 
     return this;
   }
+
+  /**
+   * @param {*} item
+   * @param {Comparator} [customFindingComparator]
+   * @return {PriorityQueue}
+   */
+  remove(item, customFindingComparator) {
+    super.remove(item, customFindingComparator);
+    delete this.priorities[item];
+
+    return this;
+  }
+
+  /**
+   * @param {*} item
+   * @param {number} priority
+   * @return {PriorityQueue}
+   */
+  changePriority(item, priority) {
+    this.remove(item, new Comparator(this.compareValue));
+    this.add(item, priority);
+
+    return this;
+  }
+
+  /**
+   * @param {*} item
+   * @return {Number[]}
+   */
+  findByValue(item){
+    return this.find(item, new Comparator(this.compareValue));
+  }
+
+  /**
+   * @param {*} item
+   * @return {boolean}
+   */
+  hasValue(item) {
+    return this.findByValue(item).length > 0;
+  }
+
+  /**
+   * @param {*} a
+   * @param {*} b
+   * @return {number}
+   */
+  comparePriority(a, b) {
+    if (this.priorities[a] === this.priorities[b]) {
+      return 0;
+    }
+
+    return this.priorities[a] < this.priorities[b] ? -1 : 1;
+  }
+
+  /**
+   * @param {*} a
+   * @param {*} b
+   * @return {number}
+   */
+  compareValue(a, b) {
+    if (a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  }
 }
